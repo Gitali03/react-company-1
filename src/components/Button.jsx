bd6222e3761f99c8cd1e713a8546237524c03326
Gitali03 <aonermtk@gmail.com>
2024-11-09 15:05:29 +0300

    added components

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+export default function Button() {
+    return (
+        <div>
+             <button className="login-button" type="submit">Enter</button>
+        </div>
+    );
+    
+}
---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+
+
+export default function Input() {
+  return (
+    <div>
+      <input type="text" placeholder="Email" name="email" />
+      <input type="password" placeholder="Password" name="password" />
+    </div>
+  );
+}
