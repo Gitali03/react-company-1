300ef52353ed94fe9a3f7534742201fc81432ebb
Gitali03 <aonermtk@gmail.com>
2024-11-10 13:31:20 +0300

    added İnputImg as a variable and responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
 import "./App.css";
 import Input from "./components/Input";
 import Button from "./components/Button";
+import inputİmg from "./assets/image.png"
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="container">
-      <img src="\image.png" alt="background-image" />
+      <img src={inputİmg} alt="background-image" />
 
       <div className="form-container">
         <form>
