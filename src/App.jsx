4a143c635f7f5bf952e82d1faa02ec0b3a7de99e
Gitali03 <aonermtk@gmail.com>
2024-11-11 13:28:25 +0300

    removed index.css

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import "./App.css";
-/*import Input from "./components/Input";
-import Button from "./components/Button";
-import inputİmg from "./assets/image.png"*/
 
 function App() {
   return (
@@ -17,22 +14,3 @@ function App() {
 }
 
 export default App;
-
-/*function App() {
-  return (
-    <div className="container">
-      <img src={inputİmg} alt="background-image" />
-
-      <div className="form-container">
-        <form>
-          <h1>LOG IN</h1>
-          <div className="divider"></div>
-          <Input></Input>
-          <Button></Button>
-        </form>
-      </div>
-    </div>
-  );
-}
-
-export default App;*/
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,8 @@
-// src/pages/Login.jsx
 import React from "react";
 import Input from "../components/Input";
 import Button from "../components/Button";
 import inputImg from "../assets/image.png";
-import styles from "./Login.module.css"; 
+import styles from "./Login.module.css";
 
 export default function Login() {
   return (
